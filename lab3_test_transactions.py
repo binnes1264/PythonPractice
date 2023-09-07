aef825366dc4c8910a70ef70f1d422e369904322
binnes1264 <101842768+binnes1264@users.noreply.github.com>
2023-09-07 13:15:11 +0200

    Update lab3_test_transactions.py

--- lab3_test_transactions.py
+++ lab3_test_transactions.py
@@ -3,11 +3,16 @@ import random
 from datetime import date
 from lab3_n01445323 import CreditCard
 
+#product names
 products = ['Aztex Coin Medallion Necklace', 'Pirate Life Graphic T-shirt', 'Treasure Map Tapestry', 'Pirates of Carribean Wall Poster', 'Davy Jones Music Box' ]
 
+#customer names
 customers = ['Jack Sparrow', 'Frederick Barbossa', 'Will Turner', 'Elizabeth Swann', 'Joshamee Gibbs']
 
+
 for x, prod in enumerate(products):
+
+    #create new order with random price and order number
     print('\n ----------------------')
     print("Welcome to the Data Analytics Store!!!")
     print("Order Placed: " + date.today().strftime('%B %d, %Y'))
@@ -15,9 +20,11 @@ for x, prod in enumerate(products):
     price = random.randrange(1, 100)
     print("Order Total: ${:.2f}".format(price))
 
+    #create random card number
     cardnum = str(random.randint(1000, 9999)) + " " + str(x) + str(random.randint(100, 999)) + " " + str(random.randint(1000, 9999)) + " " + str(random.randint(1000, 9999))
     ccard = CreditCard(cardnum, customers[x], '10/25')
 
+    #transation results
     if ccard.processOrder(price):
         print('Order successfully processed.')
         print('Thank you, {} for ordering the {}. Your {} credit card will be charged for ${:.2f}.'.format(ccard.get_cardOwner(), prod, ccard.get_cardType(), price))
